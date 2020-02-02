edfc3f5e3e46ae179c58d44e1486e5832f53aa83
Daniel Gilman <gilmanda@ucla.edu>
2020-02-01 19:20:40 -0800

    update

--- lenstronomywrapper/ModelingWorkflow/time_delays_extended_project/lens_analog_model.py
+++ lenstronomywrapper/ModelingWorkflow/time_delays_extended_project/lens_analog_model.py
@@ -228,8 +228,8 @@ class AnalogModel(object):
         if self.lens.has_satellite:
 
             n_satellites = len(self.lens.satellite_mass_model)
-
-            for n in range(0, n_satellites):
+            n_max = 2
+            for n in range(0, n_max):
 
                 rein_sat = self.lens.satellite_kwargs[n]['theta_E']
                 xsat = self.lens.satellite_kwargs[n]['center_x']
