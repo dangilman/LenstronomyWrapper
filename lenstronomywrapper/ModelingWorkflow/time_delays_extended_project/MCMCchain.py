31baa806575a188829f26e6e60aca290b548b6ba
Daniel Gilman <gilmanda@ucla.edu>
2020-05-19 12:14:33 -0700

    new mcmc extract

--- lenstronomywrapper/ModelingWorkflow/time_delays_extended_project/MCMCchain.py
+++ lenstronomywrapper/ModelingWorkflow/time_delays_extended_project/MCMCchain.py
@@ -5,6 +5,102 @@ from lenstronomywrapper.LensSystem.LensSystemExtensions.lens_maps import Residua
 import random
 
 
+class MCMCchainNew(object):
+
+    def __init__(self, original_chain):
+
+        self.mcmc_samples = original_chain.mcmc_samples
+        self.lensModel = original_chain.lensModel
+        self.kwargs_lens = original_chain.kwargs_lens
+        self.param_class = original_chain.param_class
+        self.lens_system_fit = original_chain.lens_system_fit
+        self.lens = original_chain.lens
+
+        self.modelPlot = original_chain.modelPlot
+        self.kwargs_result = original_chain.kwargs_result
+        self.lensModel_full = original_chain.lensModel_full
+        self.kwargs_lens_full = original_chain.kwargs_lens_full
+
+        self.window_size = original_chain.window_size
+        self.kwargs_data_setup = original_chain.kwargs_data_setup
+
+        self.save_name_path = original_chain.save_name_path
+
+    def extract_observables(self, n_burn_frac, n_keep):
+
+        nsamples_total = int(len(self.mcmc_samples[:, 0]))
+
+        n_start = round(nsamples_total * (1 - n_burn_frac))
+
+        if n_start < 0:
+            raise Exception('n burn too large, length of array is ' + str(nsamples_total))
+
+        chain_samples = self.mcmc_samples[n_start:nsamples_total, :]
+
+        keep_inds = random.sample(list(np.arange(1, chain_samples.shape[0])), n_keep)
+
+        chain_samples = self.mcmc_samples[keep_inds, :]
+
+        chain_process = ChainPostProcess(self.lensModel, chain_samples, self.param_class,
+                                         background_quasar=self.lens_system_fit.background_quasar)
+
+        flux_ratios, source_x, source_y = chain_process.flux_ratios(self.lens.x, self.lens.y)
+
+        macro_params = chain_process.macro_params()
+
+        arrival_times = chain_process.arrival_times(self.lens.x, self.lens.y)
+
+        relative_arrival_times = np.empty((n_keep, 3))
+        for row in range(0, n_keep):
+            relative_arrival_times[row, :] = self.lens.relative_time_delays(arrival_times[row, :])
+
+        return_kwargs_data = {'flux_ratios': flux_ratios,
+                              'time_delays': relative_arrival_times,
+                              'source_x': source_x,
+                              'source_y': source_y,
+                              'kwargs_lens_macro_fit': macro_params}
+
+        return return_kwargs_data, self.kwargs_data_setup
+
+    def maps(self):
+
+        logL = self.modelPlot._imageModel.likelihood_data_given_model(
+            source_marg=False, linear_prior=None, **self.kwargs_result)
+        ndata_points = self.modelPlot._imageModel.num_data_evaluate
+        chi2_imaging = logL * 2 / ndata_points
+
+        observed_lens = self.modelPlot._select_band(0)._data
+        modeled_lens = self.modelPlot._select_band(0)._model
+        normalized_residuals = self.modelPlot._select_band(0)._norm_residuals
+
+        reconstructed_source, coord_transform = \
+            self.modelPlot._select_band(0).source(numPix=250, deltaPix=0.025)
+
+        reconstructed_source_log = np.log10(reconstructed_source)
+
+        vmin, vmax = max(np.min(reconstructed_source_log), -5), min(np.max(reconstructed_source_log), 10)
+        reconstructed_source_log[np.where(reconstructed_source_log < vmin)] = vmin
+        reconstructed_source_log[np.where(reconstructed_source_log > vmax)] = vmax
+
+        residual_maps = ResidualLensMaps(self.lensModel_full, self.lensModel, self.kwargs_lens_full, self.kwargs_lens)
+        kappa = residual_maps.convergence(self.window_size, 250)
+
+        tdelay_res_geo, tdelay_res_grav = residual_maps.time_delay_surface_geoshapiro(self.window_size, 250,
+                                                                                      self.lens.x[0], self.lens.y[0])
+        tdelay_res_map = tdelay_res_geo + tdelay_res_grav
+
+        return_kwargs = {'chi2_imaging': chi2_imaging,
+                        'mean_kappa': np.mean(kappa),
+                         'residual_convergence': kappa,
+                         'time_delay_residuals': tdelay_res_map,
+                         'reconstructed_source': reconstructed_source,
+                         'observed_lens': observed_lens,
+                         'modeled_lens': modeled_lens,
+                         'normalized_residuals': normalized_residuals,
+                         }
+
+        return return_kwargs
+
 class MCMCchain(object):
 
     def __init__(self, save_name_path, lens_system_fit, lens, mcmc_samples, kwargs_result, kwargs_model,
--- lenstronomywrapper/ModelingWorkflow/time_delays_extended_project/extract_mcmc.py
+++ lenstronomywrapper/ModelingWorkflow/time_delays_extended_project/extract_mcmc.py
@@ -2,18 +2,25 @@ import dill as pickle
 import os
 import numpy as np
 from lenstronomywrapper.Utilities.data_util import write_data_to_file
+import sys
+from lenstronomywrapper.ModelingWorkflow.time_delays_extended_project.MCMCchain import MCMCchainNew
 
 def flux_ratio_anomaly(f1, f2):
     return f1 - f2
 
+
 def time_delay_anomaly(t1, t2):
     return t1 - t2
 
-def extract_mcmc_chain_single(pickle_dir, idx, n_burn_frac, n_keep=100):
 
+def extract_mcmc_chain_single(pickle_dir, idx, n_burn_frac, n_keep=100):
     fname = pickle_dir + '/MCMCchain_' + str(idx)
 
-    file = open(fname, 'rb')
+    if os.path.exists(fname):
+        file = open(fname, 'rb')
+    else:
+        return None, None, None, None, None, None, None, None
+
     mcmc = pickle.load(file)
 
     return_kwargs_fit, kwargs_data_fit, kwargs_data_setup = mcmc.get_output(n_burn_frac, n_keep)
@@ -34,46 +41,130 @@ def extract_mcmc_chain_single(pickle_dir, idx, n_burn_frac, n_keep=100):
     time_delay_baseline = kwargs_data_setup[key]
 
     return time_delay_baseline, flux_anomaly, time_anomaly, time_delays_model, \
-                macromodel_params, return_kwargs_fit, kwargs_data_setup, save_name_path
+           macromodel_params, return_kwargs_fit, kwargs_data_setup, save_name_path
 
-def extract_mcmc_chains(pickle_dir, idx_min, idx_max, n_burn_frac, n_keep=200, save_name_path_append=''):
+def extract_observables(pickle_dir, n, n_burn_frac, n_keep=200, save_name_path_append=None):
 
-    n = idx_min
-    for _ in range(idx_min, idx_max):
+    fname = pickle_dir + '/MCMCchain_' + str(idx)
 
-        tbaseline, f, t, tdelay_model, macro_params, kw_fit, kw_setup, save_name_path = extract_mcmc_chain_single(
-            pickle_dir, n, n_burn_frac, n_keep
-        )
-        if tbaseline is None:
-            continue
-        observed_lens = kw_fit['observed_lens']
-        modeled_lens = kw_fit['modeled_lens']
-        normalized_residuals = kw_fit['normalized_residuals']
-        residual_convergence = kw_fit['residual_convergence']
-        residual_mean_kappa = kw_fit['mean_kappa']
-        time_delay_residuals = kw_fit['time_delay_residuals']
-        reconstructed_source = kw_fit['reconstructed_source']
+    if os.path.exists(fname):
+        file = open(fname, 'rb')
+    else:
+        return None
 
-        fnames = ['tbaseline_', 'flux_anomaly_', 'time_anomaly_', 'time_delays_', 'macroparams_',
+    mcmc = pickle.load(file)
+
+    mcmc_new = MCMCchainNew(mcmc)
+
+    kwargs_data_fit, kwargs_data_setup = mcmc_new.extract_observables(n_burn_frac, n_keep)
+
+    key = 'flux_ratios'
+    flux_anomaly = np.round(flux_ratio_anomaly(kwargs_data_fit[key], kwargs_data_setup[key]), 4)
+
+    key = 'time_delays'
+    time_anomaly = np.round(time_delay_anomaly(kwargs_data_fit[key], kwargs_data_setup[key]), 4)
+    time_delays_model = kwargs_data_fit[key]
+    time_delay_baseline = kwargs_data_setup[key]
+
+    if save_name_path_append is not None:
+        outpath = mcmc.save_name_path[0:-1] + save_name_path_append + '/'
+
+    else:
+        outpath = mcmc.save_name_path
+
+    fnames = ['tbaseline_', 'flux_anomaly_', 'time_anomaly_', 'time_delays_', 'macroparams_']
+
+    arrays = [time_delay_baseline, flux_anomaly, time_anomaly, time_delays_model,
+              kwargs_data_fit['kwargs_lens_macro_fit']]
+
+    for fname, arr in zip(fnames, arrays):
+        write_data_to_file(outpath + fname + str(n) + '.txt', arr, 'w')
+
+
+def extract_maps(pickle_dir, n, save_name_path_append=None):
+
+    fname = pickle_dir + '/MCMCchain_' + str(idx)
+    print(fname)
+    if os.path.exists(fname):
+        file = open(fname, 'rb')
+    else:
+        return None
+
+    mcmc = pickle.load(file)
+
+    mcmc_new = MCMCchainNew(mcmc)
+
+    kwargs_maps = mcmc_new.maps()
+
+    logL = mcmc.modelPlot._imageModel.likelihood_data_given_model(
+        source_marg=False, linear_prior=None, **mcmc.kwargs_result)
+    ndata_points = mcmc.modelPlot._imageModel.num_data_evaluate
+    chi2_imaging = logL * 2 / ndata_points
+
+    observed_lens = kwargs_maps['observed_lens']
+    modeled_lens = kwargs_maps['modeled_lens']
+    normalized_residuals = kwargs_maps['normalized_residuals']
+    residual_convergence = kwargs_maps['residual_convergence']
+    residual_mean_kappa = kwargs_maps['mean_kappa']
+    time_delay_residuals = kwargs_maps['time_delay_residuals']
+    reconstructed_source = kwargs_maps['reconstructed_source']
+
+    if save_name_path_append is not None:
+        outpath = mcmc.save_name_path[0:-1] + save_name_path_append + '/'
+
+    else:
+        outpath = mcmc.save_name_path
+    print(outpath)
+    a=input('continue')
+    np.savetxt(outpath + 'observed_' + str(n) + '.txt', X=observed_lens)
+    np.savetxt(outpath + 'modeled_' + str(n) + '.txt', X=modeled_lens)
+    np.savetxt(outpath + 'residuals_' + str(n) + '.txt', X=normalized_residuals)
+    np.savetxt(outpath + 'kappa_' + str(n) + '.txt', X=residual_convergence)
+    np.savetxt(outpath + 'tdelayres_' + str(n) + '.txt', X=time_delay_residuals)
+    np.savetxt(outpath + 'source_' + str(n) + '.txt', X=reconstructed_source)
+
+    write_data_to_file(outpath + 'kappares_' + str(n) + '.txt', residual_mean_kappa, 'w')
+    write_data_to_file(outpath + 'chi2_imaging_' + str(n) + '.txt', chi2_imaging, 'w')
+
+def extract_mcmc_chains(pickle_dir, n, n_burn_frac, n_keep=200, save_name_path_append=None):
+
+    tbaseline, f, t, tdelay_model, macro_params, kw_fit, kw_setup, save_name_path = extract_mcmc_chain_single(
+        pickle_dir, n, n_burn_frac, n_keep
+    )
+    if tbaseline is None:
+        return
+
+    observed_lens = kw_fit['observed_lens']
+    modeled_lens = kw_fit['modeled_lens']
+    normalized_residuals = kw_fit['normalized_residuals']
+    residual_convergence = kw_fit['residual_convergence']
+    residual_mean_kappa = kw_fit['mean_kappa']
+    time_delay_residuals = kw_fit['time_delay_residuals']
+    reconstructed_source = kw_fit['reconstructed_source']
+
+    fnames = ['tbaseline_', 'flux_anomaly_', 'time_anomaly_', 'time_delays_', 'macroparams_',
               'kappares_', 'chi2_imaging_']
 
-        arrays = [tbaseline, f, t, tdelay_model,
+    arrays = [tbaseline, f, t, tdelay_model,
               macro_params, residual_mean_kappa, kw_fit['chi2_imaging']]
 
-        for fname, arr in zip(fnames, arrays):
-            write_data_to_file(save_name_path + fname + str(n) + '.txt', arr, 'w')
+    if save_name_path_append is not None:
+        outpath = save_name_path[0:-1]+ save_name_path_append +'/'
 
-        save_name_path += save_name_path_append
-        print(save_name_path)
-        np.savetxt(save_name_path + 'observed_' + str(n) + '.txt', X=observed_lens)
-        np.savetxt(save_name_path + 'modeled_' + str(n) + '.txt', X=modeled_lens)
-        np.savetxt(save_name_path + 'residuals_' + str(n) + '.txt', X=normalized_residuals)
-        np.savetxt(save_name_path + 'kappa_' + str(n) + '.txt', X=residual_convergence)
-        np.savetxt(save_name_path + 'tdelayres_' + str(n) + '.txt', X=time_delay_residuals)
-        np.savetxt(save_name_path + 'source_' + str(n) + '.txt', X=reconstructed_source)
-        n+= 1
+    else:
+        outpath = save_name_path
 
-    return None
+    for fname, arr in zip(fnames, arrays):
+        write_data_to_file(outpath + fname + str(n) + '.txt', arr, 'w')
+
+    np.savetxt(outpath + 'observed_' + str(n) + '.txt', X=observed_lens)
+    np.savetxt(outpath + 'modeled_' + str(n) + '.txt', X=modeled_lens)
+    np.savetxt(outpath + 'residuals_' + str(n) + '.txt', X=normalized_residuals)
+    np.savetxt(outpath + 'kappa_' + str(n) + '.txt', X=residual_convergence)
+    np.savetxt(outpath + 'tdelayres_' + str(n) + '.txt', X=time_delay_residuals)
+    np.savetxt(outpath + 'source_' + str(n) + '.txt', X=reconstructed_source)
+
+    return
 
 try:
     base_path = os.getenv('HOME') + '/../../../../u/flashscratch/g/gilmanda'
@@ -82,10 +173,19 @@ except:
     base_path = os.getenv('HOME') + '/Code'
     assert os.path.exists(base_path)
 
-idx = 1
-lensID = 'lens0435'
-
-extension = 'mlow8.7test_fixDdt'
+lensID = 'lens1115'
+n_keep = 200
+extension = 'control_shapelets_mlow6_fixDdt'
 fname = base_path + '/tdelay_output/raw/' + lensID + '/chains_' + extension
+i_start = int(sys.argv[1])
 
-extract_mcmc_chains(fname, 1, 4, 0.5)
+append = None
+burn_cut = 0.9
+#extract_maps(fname, i_start, append)
+extract_observables(fname, i_start, burn_cut, n_keep, append)
+
+append = '_convtest'
+burn_cut = 0.75
+#extract_maps(fname, i_start, append)
+extract_observables(fname, i_start, burn_cut, n_keep, append)
+
