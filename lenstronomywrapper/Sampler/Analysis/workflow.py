1af533afd43ad7fe42c6d4ab47d05c42464649ae
Daniel Gilman <gilmanda@ucla.edu>
2020-10-24 16:24:12 -0400

    fix delta_power_law_index bug

--- lenstronomywrapper/Sampler/Analysis/workflow.py
+++ lenstronomywrapper/Sampler/Analysis/workflow.py
@@ -1,9 +1,17 @@
 import numpy as np
+from lenstronomywrapper.Sampler.Analysis.single_lens_samples import LensSamplesRaw
 import matplotlib.pyplot as plt
 import os
 import dill
 from MagniPy.Analysis.KDE.NDdensity import *
 from MagniPy.Analysis.Visualization.triplot2 import TriPlot2
+
+from MagniPy.Workflow.mocks.B1422mock import B1422Mock
+from MagniPy.Workflow.mocks.HE0435mock import Lens0435Mock
+from MagniPy.Workflow.mocks.PSJ1606mock import Lens1606Mock
+from MagniPy.Workflow.mocks.WFI2033mock import WFI2033Mock
+from MagniPy.Workflow.mocks.WGD2038Mock import Lens2038Mock
+from MagniPy.Workflow.mocks.RXJ0911mock import Lens0911Mock
 
 sigmas_1422 = ([0.01/0.88, 0.01, 0.006/0.47, None], False)
 sigmas_0435 = ([0.05, 0.049, 0.048, 0.056], False)
@@ -13,6 +21,7 @@ sigmas_1115 = ([0.01] * 3, True)
 sigmas_0405 = ([0.04, 0.03/0.7, 0.04/1.28, 0.05/0.94], False)
 sigmas_2033 = ([0.03, 0.03/0.65, 0.02/0.5, 0.02/0.53], False)
 sigmas_0911 = ([0.04/0.56, 0.05, 0.04/0.53, 0.04/0.24], False)
+sigma_scale = 1.
 
 base = os.getenv('HOME') + '/data/sims/processed_chains/'
 
@@ -23,21 +32,37 @@ sigmas_dict = {'B1422': sigmas_1422, 'PG1115': sigmas_1115,
                'HE0435': sigmas_0435, 'PSJ1606': sigmas_1606,
               'WGD2038': sigmas_2038, 'WGDJ0405': sigmas_0405,
               'WFI2033': sigmas_2033, 'RXJ0911': sigmas_0911}
+mock_lenses = {'B1422': B1422Mock(), 'HE0435': Lens0435Mock(),
+               'PSJ1606': Lens1606Mock(), 'WFI2033': WFI2033Mock(),
+               'WGD2038': Lens2038Mock(), 'RXJ0911': Lens0911Mock()}
 
-nbins = 8
-n_keep = 100
+nbins = 12
+n_keep = 600
 
-param_names = ['sigma_sub', 'delta_power_law_index', 'shear', 'theta_E']
+param_names = ['sigma_sub', 'delta_power_law_index', 'c0', 'beta', 'LOS_normalization']
+
 samples_list_weighted = []
+samples_list_weighted_2 = []
 
-include_list = ['RXJ0911']
-extension = ''
+include_list = ['WFI2033', 'HE0435', 'B1422', 'PSJ1606',
+                'WGD2038', 'WGDJ0405', 'RXJ0911']
+include_list = ['B1422']
+extension = '_lowMhalo'
 
 names = [nb + extension for nb in names_base]
 paths_base = {name: base + name for name in names}
 
 paths = [paths_base[lens_name + extension] + '/' for lens_name in include_list]
-flux_sigmas_list = [sigmas_dict[lens_name][0] for lens_name in include_list]
+flux_sigmas_list = []
+for lens_name in include_list:
+    new = []
+    for val in sigmas_dict[lens_name][0]:
+        if val is not None:
+            new.append(val * sigma_scale)
+        else:
+            new.append(val)
+    flux_sigmas_list.append(new)
+
 uncertainty_in_ratios = [sigmas_dict[lens_name][1] for lens_name in include_list]
 
 for n, path_out in enumerate(paths):
@@ -49,12 +74,7 @@ for n, path_out in enumerate(paths):
     param_names_full = samples.param_name_list
     print(samples.ranges_dictionary)
     iter = 5
-    #new_truth = {'sigma_sub': 0.045, 'delta_power_law_index': -0.4}
-    #new_sigma = {'sigma_sub': 0.005, 'delta_power_law_index': 0.02}
-    #new_fluxes = np.array([0.88 , 1., 0.474, 0.025])
-    #samples = samples.resample_from_fluxes(new_fluxes)
-    #samples = samples.resample_from_parameters(new_truth, new_sigma)
-
+    #samples = samples.apply_param_cuts(samples, {'sigma_sub': [0., 0.1]})
     x, x_full, x_ranges, stats = samples.sample_with_flux_uncertainties(
         param_names, flux_sigmas_list[n], uncertainty_in_ratios[n], iter=iter, n_keep=n_keep, auto_range_sigma=3.5)
 
@@ -62,17 +82,32 @@ for n, path_out in enumerate(paths):
     print(np.max(stats), stats.shape)
     print(x.shape)
     print(x_ranges)
-    #x_ranges = [[0, 0.2], [-0.8, 0.8]]
 
-    weight_names = ['center_x', 'center_y', 'power_law_index']
-    mean_list = [0., 0., -1.9]
-    sigma_list = [0.05, 0.05, 0.05]
-    weights = [samples.gaussian_weight(x_full, weight_names,
-                                       mean_list, sigma_list)]
+    #x_ranges = [[-0.5, 0.8], [0, 1.25], [0., 2], [0, 2]]
+
+    weight_names_hyper = ['power_law_index']
+    weight_names = ['center_x', 'center_y']
+
+    mean_list_hyper = [-1.9]
+    sigma_list_hyper = [0.05]
+
+    mean_list = [0.0, 0.0]
+    sigma_list = [0.05, 0.05]
+
+    apply_hyper = [0]
+
+    if n in apply_hyper:
+        weights = [samples.gaussian_weight(
+            x_full, weight_names_hyper + weight_names,
+            mean_list_hyper + mean_list, sigma_list_hyper + sigma_list)]
+
+    else:
+        weights = [samples.gaussian_weight(
+            x_full, weight_names, mean_list, sigma_list)]
 
     samples_list_weighted.append(DensitySamples([x], param_names, weights,
                                        param_ranges=x_ranges, nbins=nbins,
-                                       use_kde=False, bwidth_scale=0.4))
+                                       use_kde=True, bwidth_scale=0.3))
 
 sim_weighted = IndepdendentDensities(samples_list_weighted)
 
@@ -80,9 +115,10 @@ levels = [0.05, 0.32, 1.]
 
 triplot = TriPlot2([sim_weighted], param_names, x_ranges)
 triplot.truth_color = 'b'
-truths = None
-out = triplot.make_triplot(param_names=param_names, truths=truths,
+
+out = triplot.make_triplot(param_names=param_names, truths=None,
                      filled_contours=False, show_intervals=False,
                      show_contours=True, levels=levels)
+#plt.savefig('mock_8.pdf')
 
 plt.show()
