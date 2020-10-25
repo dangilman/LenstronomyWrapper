89b9dfb4031e6bcd92ae81f272127cc2e66d83a1
Daniel Gilman <gilmanda@ucla.edu>
2020-10-24 21:24:21 -0400

    update

--- lenstronomywrapper/Sampler/launch_script.py
+++ lenstronomywrapper/Sampler/launch_script.py
@@ -1,12 +1,13 @@
 from lenstronomywrapper.Sampler.run import run
+from lenstronomywrapper.Sampler.run_two_sources import run_two_sources
 import sys
 import os
 from time import time
 
-#job_index = int(sys.argv[1])
-job_index = 1
+job_index = int(sys.argv[1])
+#job_index = 1
 # the name of the folder containing paramdictionary files
-chain_ID = 'B1422'
+chain_ID = 'b1422_analog_diffsrc'
 # where to generate output files
 #out_path = '/scratch/abenson/'
 out_path = os.getenv('HOME') + '/data/sims/'
@@ -19,7 +20,8 @@ paramdictionary_folder_path = os.getenv('HOME') + '/data/'
 print(job_index)
 t0 = time()
 # launch and forget
-run(job_index, chain_ID, out_path,
-    paramdictionary_folder_path, test_mode=True)
+test_mode = False
+run_two_sources(job_index, chain_ID, out_path,
+    paramdictionary_folder_path, test_mode=test_mode)
 tend = time()
 print('time ellapsed: ', tend - t0)
